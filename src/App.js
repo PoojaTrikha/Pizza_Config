f2a737889af6c9331750f1d344467949d97887d4
Pooja <pooja.kaul2015@gmail.com>
2018-09-08 17:11:26 +0200

    pizza

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import './App.css';
 import Base from './components/Base'
 import Sauce from './components/Sauce'
 import CheckBox from './components/CheckBox'
