56f2941cc2eca1dd07d566f9f3a44924179e2c20
Pooja <pooja.kaul2015@gmail.com>
2018-09-08 16:47:24 +0200

    pizza

--- src/components/PizzaPrice.js
+++ src/components/PizzaPrice.js
@@ -5,12 +5,11 @@ class PizzaPrice extends Component {
     render() {
         const price = Number(this.props.PizzaSelected.baseprice) + Number(this.props.PizzaSelected.sauceprice) + Number(this.props.PizzaSelected.toppings.length * 0.50)
         const turborprice = (0.1 * (Number(this.props.PizzaSelected.baseprice) + Number(this.props.PizzaSelected.sauceprice) + Number(this.props.PizzaSelected.toppings.length * 0.50)))
-
         return (
             <div>
-                <label className="price">Price</label>
+                <label className="price">Total Payable Amount</label>
                 {(this.props.PizzaSelected.turbodelivery)
-                    ? <input className="priceinput" value={Number(price + turborprice).toFixed(2)} type={'text'}/>
+                    ? <input className="priceinput" value={Number(price + turborprice)} type={'text'}/>
                     : <input className="priceinput" value={price} type={'text'}/>}
             </div>
         )
